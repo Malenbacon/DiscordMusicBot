93bdf515c810e5add2f9b02360dfa567da407b2d
Wallace <wallacegomes6877@gmail.com>
2024-08-24 08:42:00 -0300

    feat: add help command

--- commands/help.js
+++ commands/help.js
@@ -1,7 +1,14 @@
+let grabAllCommands = require("../services/grabAllCommands")
 module.exports = {
     name: "!help",
     help:"apresenta todos os comandos",
     async execute(interaction){
+        let allCommands = grabAllCommands();
+        let commandsStringBuilder = '';
+        for(command of allCommands){
+            commandsStringBuilder += `${command[1].name} - ${command[1].help} \n`
+        }
+        return interaction.reply(commandsStringBuilder);
         
     }
 };

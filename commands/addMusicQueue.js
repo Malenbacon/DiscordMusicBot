46e29d65eb4db1ed03474971b76b3580e72b91ac
Wallace <wallacegomes6877@gmail.com>
2024-08-25 19:58:23 -0300

    feat: support to attachments files

--- commands/addMusicQueue.js
+++ commands/addMusicQueue.js
@@ -19,22 +19,39 @@ module.exports = {
     name: "!play",
     help:"Dado um url do youtube, adiciona essa musica a fila",
     async execute(interaction){
-        if(interaction.member.voice.channelId === null) return interaction.reply("Entre em um canal de voz primeiro");
-        let menssageStriped = interaction.content.split(" ");
-        if(menssageStriped[1] == undefined) return interaction.reply("Voce deve oferecer um link do youtube valido após o add");
-        if(checkIfIsYoutubeDomain(menssageStriped[1])){
+        const [attachments] = interaction.attachments.values();
+        if(!attachments.url){
+            if(interaction.member.voice.channelId === null) return interaction.reply("Entre em um canal de voz primeiro");
+            let menssageStriped = interaction.content.split(" ");
+            if(menssageStriped[1] == undefined) return interaction.reply("Voce deve oferecer um link do youtube valido após o add");
+            if(checkIfIsYoutubeDomain(menssageStriped[1])){
+                if(queueMusics.length === 0){
+                queueMusics.push(menssageStriped[1]);
+                addEvent.emit('AddedToQueueEmpty', interaction);
+                return;
+                }
+                else if(queueMusics.length !== 0){
+                    queueMusics.push(menssageStriped[1]);
+                    addEvent.emit('AddedToQueue', interaction);
+                }
+            } 
+            else {
+                return interaction.reply("O link deve ser do youtube!")
+            };
+        }
+        if(attachments.url){
+            if(interaction.member.voice.channelId === null) return interaction.reply("Entre em um canal de voz primeiro");
+            
             if(queueMusics.length === 0){
-            queueMusics.push(menssageStriped[1]);
-            addEvent.emit('AddedToQueueEmpty', interaction);
-            return;
+                queueMusics.push(attachments.url);
+                addEvent.emit('AddedToQueueEmpty', interaction);
+                return;
             }
             else if(queueMusics.length !== 0){
-                queueMusics.push(menssageStriped[1]);
-                addEvent.emit('AddedToQueue');
+                queueMusics.push(attachments.url)
+                addEvent.emit('AddedToQueue', interaction);
+                return;
             }
-        } 
-        else {
-            return interaction.reply("O link deve ser do youtube!")
-        };
-    }, addEvent
+        }
+    }
 };
